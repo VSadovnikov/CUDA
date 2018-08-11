51b552cc8ef1eb9889a1040bde680c17add91d7f
VSadovnikov <basilsad1989@gmail.com>
2018-08-11 17:45:44 +0300

    fix responsive

--- js/script.js
+++ js/script.js
@@ -124,65 +124,123 @@ $('.slider').each(function() {
 });
 
 
-(function() {
-  $(document).ready(function() {
-    $('.skill-box').find('b').each(function(i) {
-      return $(this).prop('Counter', 0).animate({
-        Counter: $(this).parent().data('percent')
-      }, {
-        duration: 3000,
-        easing: 'swing',
-        step: function(now) {
-          return $(this).text(Math.ceil(now) + '%');
+document.addEventListener("DOMContentLoaded", function() {
+
+  var circleProgress = (function(selector) {
+    var wrapper = document.querySelectorAll(selector);
+    Array.prototype.forEach.call(wrapper, function(wrapper, i) {
+      var wrapperWidth,
+        wrapperHeight,
+        percent,
+        innerHTML,
+        context,
+        lineWidth,
+        centerX,
+        centerY,
+        radius,
+        newPercent,
+        speed,
+        from,
+        to,
+        duration,
+        start,
+        strokeStyle,
+        text;
+
+      var getValues = function() {
+        wrapperWidth = parseInt(window.getComputedStyle(wrapper).width);
+        wrapperHeight = wrapperWidth;
+        percent = wrapper.getAttribute('data-cp-percentage');
+        innerHTML = '<span class="percentage"><strong>' + percent + '</strong> %</span><canvas class="circleProgressCanvas" width="' + (wrapperWidth * 2) + '" height="' + wrapperHeight * 2 + '"></canvas>';
+        wrapper.innerHTML = innerHTML;
+        text = wrapper.querySelector(".percentage");
+        canvas = wrapper.querySelector(".circleProgressCanvas");
+        wrapper.style.height = canvas.style.width = canvas.style.height = wrapperWidth + "px";
+        context = canvas.getContext('2d');
+        centerX = canvas.width / 2;
+        centerY = canvas.height / 2;
+        newPercent = 0;
+        speed = 1;
+        from = 0;
+        to = percent;
+        duration = 1000;
+        lineWidth = 25;
+        radius = canvas.width / 2 - lineWidth;
+        strokeStyle = wrapper.getAttribute('data-cp-color');
+        start = new Date().getTime();
+      };
+
+      function animate() {
+        requestAnimationFrame(animate);
+        var time = new Date().getTime() - start;
+        if (time <= duration) {
+          var x = easeInOutQuart(time, from, to - from, duration);
+          newPercent = x;
+          text.innerHTML = Math.round(newPercent) + " %";
+          drawArc();
         }
+      }
+
+      function drawArc() {
+        var circleStart = 1.5 * Math.PI;
+        var circleEnd = circleStart + (newPercent / 50) * Math.PI;
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.beginPath();
+        context.arc(centerX, centerY, radius, circleStart, 4 * Math.PI, false);
+        context.lineWidth = lineWidth;
+        context.strokeStyle = "#ddd";
+        context.stroke();
+        context.beginPath();
+        context.arc(centerX, centerY, radius, circleStart, circleEnd, false);
+        context.lineWidth = lineWidth;
+        context.strokeStyle = strokeStyle;
+        context.stroke();
+
+      }
+      var update = function() {
+        getValues();
+        animate();
+      }
+      update();
+
+      var btnUpdate = document.querySelectorAll(".btn-update")[0];
+      btnUpdate.addEventListener("click", function() {
+        wrapper.setAttribute("data-cp-percentage", Math.round(getRandom(5, 95)));
+        update();
+      });
+      wrapper.addEventListener("click", function() {
+        update();
+      });
+
+      var resizeTimer;
+      window.addEventListener("resize", function() {
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(function() {
+          clearTimeout(resizeTimer);
+          start = new Date().getTime();
+          update();
+        }, 250);
       });
     });
-    return $('.skill-box .skills-circle li').each(function(i) {
-      var _left, _percent, _right, deg, full_deg, run_duration;
-      _right = $(this).find('.bar-circle-right');
-      _left = $(this).find('.bar-circle-left');
-      _percent = $(this).attr('data-percent');
-      deg = 3.6 * _percent;
-      if (_percent <= 50) {
-        return _right.animate({
-          circle_rotate: deg
-        }, {
-          step: function(deg) {
-            $(this).css('transform', 'rotate(' + deg + 'deg)');
-          },
-          duration: 1000
-        });
-      } else {
-        full_deg = 180;
-        deg -= full_deg;
-        run_duration = 1000 * (50 / _percent);
-        return _right.animate({
-          circle_rotate: full_deg
-        }, {
-          step: function(full_deg) {
-            $(this).css('transform', 'rotate(' + full_deg + 'deg)');
-          },
-          duration: run_duration,
-          easing: 'linear',
-          complete: function() {
-            run_duration -= 1000;
-            _left.css({
-              'clip': 'rect(0, 150px, 150px, 75px)',
-              'background': '#B0DAB9'
-            });
-            return _left.animate({
-              circle_rotate: deg
-            }, {
-              step: function(deg) {
-                $(this).css('transform', 'rotate(' + deg + 'deg)');
-              },
-              duration: Math.abs(run_duration),
-              easing: 'linear'
-            });
-          }
-        });
-      }
-    });
+
+    //
+    // http://easings.net/#easeInOutQuart
+    //  t: current time
+    //  b: beginning value
+    //  c: change in value
+    //  d: duration
+    //
+    function easeInOutQuart(t, b, c, d) {
+      if ((t /= d / 2) < 1) return c / 2 * t * t * t * t + b;
+      return -c / 2 * ((t -= 2) * t * t * t - 2) + b;
+    }
+
   });
 
-}).call(this);
+  circleProgress('.counter');
+
+  // Gibt eine Zufallszahl zwischen min (inklusive) und max (exklusive) zurück
+  function getRandom(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+});
